f97f890eaa7c29d73b96239c2869bb666473dff8
DYoshizawa079 <daniely079@gmail.com>
2022-01-21 21:13:36 -0500

    Added models

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const Product = require('./Product.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
+    
+
   },
   {
     sequelize,
